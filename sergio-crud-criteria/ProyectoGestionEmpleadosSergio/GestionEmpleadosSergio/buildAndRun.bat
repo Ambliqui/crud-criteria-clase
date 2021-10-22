a5979d425b8290e9d15cc22e693318a34f3cac84
Pedro Pérez <pedro@zepyrshut.com>
2021-10-22 13:07:21 +0200

    correccion fallo directorios

--- sergio-crud-criteria/ProyectoGestionEmpleadosSergio/GestionEmpleadosSergio/buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t SLC/GestionEmpleados .
-call docker rm -f GestionEmpleados
-call docker run -d -p 9080:9080 -p 9443:9443 --name GestionEmpleados SLC/GestionEmpleados
--- sergio-crud-criteria/ProyectoGestionEmpleadosSergio/GestionEmpleadosSergio/src/main/webapp/include/Titulo.jsp
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="css/TituloCSS.css">
-<header>
-    <a href="Index.jsp">
-        <img id="logo" src="imagenes/Logo.png">
-    </a>
-    <h1>GESTOR DE EMPLEADOS SLC.SA</h1>
-</header>
